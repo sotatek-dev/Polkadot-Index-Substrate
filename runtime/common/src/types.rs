008b8d984817a0ef9bf8472dea9c3dd3d2b8608e
Bao Hoang <bao.hoang2@sotatek.com>
2022-07-19 11:50:11 +0700

    add missing library standard

--- runtime/common/src/types.rs
+++ runtime/common/src/types.rs
@@ -3,10 +3,9 @@
 use sp_std::marker::PhantomData;
 
 /// Origin either `Root` or `CommitteeOrigin`
-pub type GovernanceOrigin<AccountId, Runtime> = frame_system::EnsureOneOf<
-	AccountId,
-	pallet_committee::EnsureApprovedByCommittee<Runtime>,
-	frame_system::EnsureRoot<AccountId>,
+pub type GovernanceOrigin<AccountId, Runtime> = frame_support::traits::EnsureOneOf<
+    pallet_committee::EnsureApprovedByCommittee<Runtime>,
+    frame_system::EnsureRoot<AccountId>,
 >;
 
 /// Origin that approved by committee
@@ -15,25 +14,29 @@ pub type CommitteeOrigin<Runtime> = pallet_committee::EnsureApprovedByCommittee<
 /// Range of voting period
 pub struct VotingPeriodRange<T>(PhantomData<T>);
 
-impl<T: frame_system::Config> pallet_committee::traits::VotingPeriodRange<T::BlockNumber> for VotingPeriodRange<T> {
-	fn max() -> T::BlockNumber {
-		(crate::constants::DAYS * 28).into()
-	}
+impl<T: frame_system::Config> pallet_committee::traits::VotingPeriodRange<T::BlockNumber>
+    for VotingPeriodRange<T>
+{
+    fn max() -> T::BlockNumber {
+        (crate::constants::DAYS * 28).into()
+    }
 
-	fn min() -> T::BlockNumber {
-		(crate::constants::DAYS * 7).into()
-	}
+    fn min() -> T::BlockNumber {
+        (crate::constants::DAYS * 7).into()
+    }
 }
 
 /// Range of lockup period
 pub struct LockupPeriodRange<T>(PhantomData<T>);
 
-impl<T: frame_system::Config> pallet_asset_index::traits::LockupPeriodRange<T::BlockNumber> for LockupPeriodRange<T> {
-	fn min() -> T::BlockNumber {
-		crate::constants::DAYS.into()
-	}
+impl<T: frame_system::Config> pallet_asset_index::traits::LockupPeriodRange<T::BlockNumber>
+    for LockupPeriodRange<T>
+{
+    fn min() -> T::BlockNumber {
+        crate::constants::DAYS.into()
+    }
 
-	fn max() -> T::BlockNumber {
-		(crate::constants::DAYS * 28).into()
-	}
+    fn max() -> T::BlockNumber {
+        (crate::constants::DAYS * 28).into()
+    }
 }
